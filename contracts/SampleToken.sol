9c92fcd1945bb22d0c33c1f366dedfc7414c0be3
Latricia <tricianickelberry@gmail.com>
2021-06-20 09:55:49 -0700

    added note

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.24;
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract SampleToken is ERC20Detailed, ERC20 {
+contract SampleToken is ERC20Detailed, ERC20 {  // sample contract 
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint _initialSupply) 
     ERC20Detailed(_name, _symbol, _decimals) public {
